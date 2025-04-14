ab4d1bd78ca1cc1651cd3335991e5b845aef9dc7
Gaber-eg <133911723+Gaber-dev@users.noreply.github.com>
2025-04-14 22:34:29 +0200

    Add files via upload

---
+++ Day 6/Trainee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_6
+{
+    class Trainee : Person
+    {
+        public string NID { get; set; }
+        public int IntakeNumber { get; set; }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}, NID: {NID}, IntakeNumber: {IntakeNumber}";
+        }
+
+        public override Person Clone()
+        {
+            Person c = (Person)this.MemberwiseClone();
+
+            // City , street , zipcode
+
+
+            c.address = new Address();
+            c.address.City = this.address.City;
+            c.address.Street = this.address.Street;
+            c.address.ZipCode = this.address.ZipCode;
+
+            return c;
+        }
+    }
+}
