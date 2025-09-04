149de3154522e1511890c6694ad82d07d04489ac
Gaber-eg <133911723+Gaber-dev@users.noreply.github.com>
2025-09-04 13:13:14 +0300

    Add files via upload

---
+++ Day3/Line.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    public class Line
+    {
+        _2DPoint point1;
+        _2DPoint point2;
+
+        public Line(int _p1x, int _p1y, int _p2x, int _p2y)
+        {
+            point1 = new _2DPoint(_p1x, _p1y);
+            point2 = new _2DPoint(_p2x, _p2y);
+        }
+
+
+        public int GetLength()
+        {
+            return Convert.ToInt32( Math.Sqrt(Math.Pow((point1.GetX() - point2.GetX()), 2) + Math.Pow((point1.GetY() - point2.GetY()), 2)));
+        } 
+    }
+}
---
+++ Day3/Square.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    public class Square : Rectangle
+    {
+        public Square(Line _l)
+        {
+            l1 = _l;
+            l2 = null;
+        }
+
+        public int getLength()
+        {
+            return l1.GetLength();
+        }
+        public int GetArea()
+        {
+            int length = l1.GetLength();
+            int area = length * length;
+
+            return area;
+        }
+    }
+}
